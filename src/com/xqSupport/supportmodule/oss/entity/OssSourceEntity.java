3c517c1a9a24b7a8155279e25894db72c8e7256a
fangxu_macPro <mac@MacdeMacBook-Pro.local>
2018-01-24 22:43:56 +0800

    sync

--- src/com/xqSupport/supportmodule/oss/entity/OssSourceEntity.java
+++ src/com/xqSupport/supportmodule/oss/entity/OssSourceEntity.java
@@ -5,12 +5,14 @@ import com.xqSupport.Entity.BaseEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 /**
  * Created by mac on 2018/1/21.
  */
 @Entity
-@Table(name = "Service_Oss_Source", schema = "xq", catalog = "")
+@Table(name = "Service_Oss_Source", schema = "xq", catalog = "",
+        uniqueConstraints = {@UniqueConstraint(columnNames = "sourceKey")})
 public class OssSourceEntity extends BaseEntity {
     public String getSourceKey() {
         return sourceKey;
